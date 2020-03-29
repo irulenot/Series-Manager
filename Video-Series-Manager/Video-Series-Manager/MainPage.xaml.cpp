dc42a8bbb7c3b61b54cff34de8bc26d08a24f18d
Anthony Bilic <anthony.i.bilic@gmail.com>
2020-03-28 19:21:10 -0700

    Adding new C++ Project

---
+++ Video-Series-Manager/Video-Series-Manager/MainPage.xaml.cpp
@@ -0,0 +1,39 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include <string>
+#include <iostream>
+
+using namespace Video_Series_Manager;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void Video_Series_Manager::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	button1->Content = "Morph";
+	const char* command = "echo TEST && pause";
+	system(command);
+}
